fbf05d77cc59849ae8446b51a7e2d297f7700379
wangdaojian <419550295@qq.com>
2017-06-17 17:58:43 +0800

    框架搭建

--- src/main/java/com/bupt/flowpackage/biz/auth/controller/LoginController.java
+++ src/main/java/com/bupt/flowpackage/biz/auth/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.bupt.flowpackage.biz.auth.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -8,6 +10,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 @Controller
 public class LoginController {
+	public static Logger logger = LoggerFactory.getLogger(LoginController.class);
 	private static final String LOGIN_PAGE ="home";
 	
 	@ResponseBody
@@ -18,6 +21,11 @@ public class LoginController {
 	
 	@RequestMapping("/index")
 	public ModelAndView index() {
+		try{
+			int i = 1/0;
+		}catch(Exception e) {
+			logger.error("访问首页失败！", e);
+		}
 		ModelAndView mv = new ModelAndView(LOGIN_PAGE);
 		return mv;
 	}
